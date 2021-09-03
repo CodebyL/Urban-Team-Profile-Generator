2f7e0dd7f14b0d194d528349a9b6e79a9ae13a47
Lisette Colon <lisettecolon23@gmail.com>
2021-09-03 16:27:01 -0400

    Json, Js, CSS

---
+++ Src/Questions.js
@@ -0,0 +1,78 @@
+
+module.exports = {
+
+    managerQuestions: [
+           {
+            type: "input",
+            name: "Manager",
+            message: "Please enter your Manager's name.",
+        },
+        {
+            type: "input",
+            name: "ManagerID",
+            message: "Please enter your Manager's ID number",
+        },
+        {
+            type: "input",
+            name: "ManagerEmail",
+            message: "Please enter your Manager's email.",
+        },
+        {
+            type:"input",
+            name:"ManagerOfficeNumber",
+            message: "Please enter your Manager's office number.",
+        }
+    ],
+    
+    internQuestions: [
+        {
+            type: "input",
+            name: "InternName",
+            message: "Please enter your Intern's name.",
+        },
+        {
+            type: "input",
+            name: "InternID",
+            message: "Please enter your Intern's ID number",
+        },
+        {
+            type: "input",
+            name: "InternEmail",
+            message: "Please enter your Intern's email.",
+        },
+        {
+            type: "input",
+            name: "InternSchool",
+            message: "Please enter your Intern's school name.",
+        }
+    ],
+    engineerQuestions: [
+        {
+            type: "input",
+            name: "EngineerName",
+            message: "Please enter your Engineer's name.",
+        },
+        {
+            type: "input",
+            name: "EngineerID",
+            message: "Please enter your Engineer's ID number",
+        },
+        {
+            type: "input",
+            name: "EngineerEmail",
+            message: "Please enter your Engineer's email.",
+        },
+        {
+            type: "input",
+            name: "EngineerGithub",
+            message: "Please enter your Engineer's Github.",
+        }
+    ],
+    mainMenuQuestions: [
+        {
+            type: "input",
+            name: "mainmenu",
+            message: "What would you like to do?",
+            },    
+        ],
+    }
